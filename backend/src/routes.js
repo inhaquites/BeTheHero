6cff0cea60823f960c92b650e750c119814b15b3
Rodrigo Inhaquites <rodrigo.inhaquites@hammerconsult.com.br>
2020-03-28 01:45:50 -0300

    Backend da aplicacao

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const OngController = require('./Controllers/OngController');
+const IncidentController = require('./Controllers/IncidentController');
+const ProfileController = require('./Controllers/ProfileController');
+const SessionController = require('./Controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+
+module.exports = routes;
