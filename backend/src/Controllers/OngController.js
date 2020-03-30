c8ee5032a284e9d1a0b199b3d818471f16aafe7c
Rodrigo Inhaquites <rodrigo.inhaquites@hammerconsult.com.br>
2020-03-30 20:43:04 -0300

    Mobile da aplicação

--- backend/src/Controllers/OngController.js
+++ backend/src/Controllers/OngController.js
@@ -1,5 +1,7 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
+
+
 
 module.exports = {
   async index (req, res)  {
@@ -11,7 +13,7 @@ module.exports = {
   async create(req, res) {
     const {name, email, whatsapp, city, uf} = req.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
